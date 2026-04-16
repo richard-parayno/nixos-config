81448d1e69d6c4718123558ff146b8f4af6bebf8
Richard Parayno <broodheart@gmail.com>
2026-04-16 23:14:03 +0800

    update flake

--- modules/system/common-apps.nix
+++ modules/system/common-apps.nix
@@ -2,6 +2,19 @@
 {
   flake.nixosModules.common-apps =
     { pkgs, ... }:
+    let
+      # Electron defaults to the unsupported/basic_text backend under Niri because
+      # XDG_CURRENT_DESKTOP is "niri", even when gnome-keyring is available.
+      # Force Element to use libsecret so it can use the running keyring.
+      element-desktop-with-keyring = pkgs.runCommand "element-desktop-with-keyring" {
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+      } ''
+        mkdir -p "$out/bin" "$out/share"
+        cp -rs ${pkgs.element-desktop}/share/. "$out/share/"
+        makeWrapper ${pkgs.element-desktop}/bin/element-desktop "$out/bin/element-desktop" \
+          --add-flags "--password-store=gnome-libsecret"
+      '';
+    in
     {
       environment.systemPackages = with pkgs; [
         discord
@@ -13,6 +26,8 @@
         obsidian
         ytmdesktop
         bitwarden-desktop
+        cinny-desktop
+        element-desktop-with-keyring
       ];
 
     };
